2738111873d40f1bbe6dadfa2f05f053f709bfba
Benjamin Vokes <ben.mym.ac>
2019-12-08 08:59:02 -0800

    Added ten questions.

--- webquiz.js
+++ webquiz.js
@@ -1,4 +1,77 @@
 var questions = [{
+    // Question one...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question two...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question three...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question four...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question five...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question six...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question seven...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question eight...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question nine...
+    "question": "What is two + two ?",
+    "option1": "three",
+    "option2": "four",
+    "option3": "five",
+    "option4": "six",
+    "answer": "2"
+}, {
+    // Question ten...
     "question": "What is two + two ?",
     "option1": "three",
     "option2": "four",
